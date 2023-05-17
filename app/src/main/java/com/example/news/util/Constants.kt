afc8a48f371be51249ec07397c3fa5f970d2917c
Vishal Singh <123194054+vishalsingh444@users.noreply.github.com>
2023-05-17 12:47:04 +0530

    Update Constants.kt

--- app/src/main/java/com/example/news/util/Constants.kt
+++ app/src/main/java/com/example/news/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.news.util
 
 class Constants {
     companion object{
-        const val API_KEY = "943228ab5b5645b7b4178ed16070d02f"
+        const val API_KEY = "APIkey"
         const val BASE_URL = "https://newsapi.org"
     }
 }
